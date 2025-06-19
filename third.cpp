7ff125a6d71b7e5cc956802a1db9e62f9dc23810
Nabin Yadav <nabinyadav0010@gmail.com>
2025-06-18 21:40:58 -0400

    new project

---
+++ third.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float p,t,r,s;
+
+    cout << "Enter principle" ;
+    cin >> p;
+
+    cout << "Enter rate" ;
+    cin >> r;
+
+    cout << "Enter time" ;
+    cin >> t;
+
+    s= (p*t*r)/100;
+
+    cout << "Simple interest : " << s << endl;
+
+    return 0;
+
+}
